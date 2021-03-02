8a41bd6de29d836980862561ae6f57b9a32cb6d9
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-03-02 17:59:43 +0000

    Add layout class

---
+++ src/main/java/mccreery/betterhud/api/layout/HudLayout.java
@@ -0,0 +1,29 @@
+package mccreery.betterhud.api.layout;
+
+import mccreery.betterhud.api.HudElement;
+import mccreery.betterhud.api.HudRenderContext;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HudLayout {
+    private final Set<HudElement> rootElements = new HashSet<>();
+
+    public Set<HudElement> getRootElements() {
+        return rootElements;
+    }
+
+    public void render(HudRenderContext context) {
+        for (HudElement element : rootElements) {
+            renderTree(element, context);
+        }
+    }
+
+    private void renderTree(HudElement element, HudRenderContext context) {
+        element.render(context);
+
+        for (HudElement child : element.getChildren()) {
+            renderTree(child, context);
+        }
+    }
+}
