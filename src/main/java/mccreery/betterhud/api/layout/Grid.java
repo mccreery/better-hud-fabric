d8a2a4c3987642fa833ea9935bb8a015e0abff5c
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-03-28 23:13:34 +0100

    Rewrite grid size and rendering

--- src/main/java/mccreery/betterhud/api/layout/Grid.java
+++ src/main/java/mccreery/betterhud/api/layout/Grid.java
@@ -1,6 +1,7 @@
 package mccreery.betterhud.api.layout;
 
 import mccreery.betterhud.api.geometry.Point;
+import mccreery.betterhud.api.geometry.Rectangle;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -15,46 +16,41 @@ public class Grid extends LayoutBox {
     private double rowGutter;
     private double columnGutter;
 
-    private boolean stretch = false;
-    private Direction alignment = Direction.NORTH_WEST;
-    private Direction cellAlignment = Direction.CENTER;
+    private boolean stretch;
+    private Point cellAlignment = Rectangle.Node.CENTER.getT();
 
     public Grid(int rowCount, int columnCount) {
+        if (rowCount < 1 || columnCount < 1) {
+            throw new IllegalArgumentException("Minimum 1 row and 1 column");
+        }
+
         this.rowCount = rowCount;
         this.columnCount = columnCount;
-        this.cells = new ArrayList<>(Collections.nCopies(rowCount * columnCount, null)));
-    }
-
-    /**
-     * Converts the row count and column count to a double-precision point.
-     * @return The size of the grid in cells.
-     */
-    private Point getShape() {
-        return new Point(columnCount, rowCount);
-    }
-
-    /**
-     * Converts the row and column gutters to a point.
-     * @return The size of the gutter.
-     */
-    private Point getGutter() {
-        return new Point(columnGutter, rowGutter);
-    }
-
-    public double getRowGutter() {
-        return rowGutter;
+        this.cells = new ArrayList<>(Collections.nCopies(rowCount * columnCount, null));
     }
 
     public void setRowGutter(double rowGutter) {
         this.rowGutter = rowGutter;
     }
 
-    public double getColumnGutter() {
-        return columnGutter;
-    }
-
     public void setColumnGutter(double columnGutter) {
         this.columnGutter = columnGutter;
+    }
+
+    public void setStretch(boolean stretch) {
+        this.stretch = stretch;
+    }
+
+    /**
+     * Sets the cell alignment.
+     * @param t An interpolation parameter.
+     */
+    public void setCellAlignment(Point t) {
+        this.cellAlignment = t;
+    }
+
+    public void setCellAlignment(Rectangle.Node node) {
+        setCellAlignment(node.getT());
     }
 
     public LayoutBox getCell(int row, int column) {
@@ -74,90 +70,53 @@ public class Grid extends LayoutBox {
         return row * columnCount + column;
     }
 
-    public Rect getCellBounds(Rect bounds, Point position) {
-        Size gutterless = bounds.getSize().sub(shape.getSize().sub(1, 1).scale(gutter));
-        Size cellSize = gutterless.scale(1.0f / shape.getWidth(), 1.0f / shape.getHeight());
-
-        Direction flow = alignment.mirror();
-        Point offset = new Point((flow.getCol() - 1) * position.getX(), (flow.getRow() - 1) * position.getY()).scale(cellSize.add(gutter));
-        return new Rect(cellSize).anchor(bounds, alignment).translate(offset);
-    }
-
-    public boolean hasStretch() {
-        return stretch;
-    }
-
-    public Grid<T> setStretch(boolean stretch) {
-        this.stretch = stretch;
-        return this;
-    }
-
-    public Direction getAlignment() {
-        return alignment;
-    }
-
-    public Grid<T> setAlignment(Direction alignment) {
-        if(!DirectionOptions.CORNERS.isValid(alignment)) {
-            throw new IllegalArgumentException("Grid alignment must be a corner");
-        }
-        this.alignment = alignment;
-        return this;
-    }
-
-    public Direction getCellAlignment() {
-        return cellAlignment;
-    }
-
-    public Grid<T> setCellAlignment(Direction cellAlignment) {
-        this.cellAlignment = cellAlignment;
-        return this;
-    }
-
     @Override
-    public Size getPreferredSize() {
-        if(shape.isEmpty()) return Size.zero();
+    public Point getPreferredSize() {
+        Point cellSize = getCellSize();
+        Point gutter = getGutter();
+        Point shape = getShape();
 
-        int width = 0, height = 0;
-
-        for(Boxed element : flatten()) {
-            if(element == null) continue;
-            Size size = element.getPreferredSize();
-
-            if(size.getWidth() > width) width = size.getWidth();
-            if(size.getHeight() > height) height = size.getHeight();
-        }
-
-        return shape.getSize().scale(width, height)
-            .add(shape.getSize().sub(1, 1).scale(gutter));
+        return cellSize.add(gutter).scale(shape).subtract(gutter);
     }
 
     @Override
     public void render() {
-        int x = 0;
+        Point cellSize = getCellSize();
+        Point cellStep = cellSize.add(getGutter());
+        Rectangle bounds = getBounds();
 
-        Size gutterless = bounds.getSize().sub(shape.getSize().sub(1, 1).scale(gutter));
-        Size cellSize = gutterless.scale(1.0f / shape.getWidth(), 1.0f / shape.getHeight());
+        for (int row = 0; row < rowCount; row++) {
+            for (int column = 0; column < columnCount; column++) {
+                LayoutBox cell = getCell(row, column);
 
-        Rect cellLeft = new Rect(cellSize).anchor(bounds, alignment);
-        Rect cell = cellLeft;
-
-        Rect gutterPadding = new Rect(gutter.invert(), new Point(gutter));
-        Direction flow = alignment.mirror();
-
-        for(Boxed element : flatten()) {
-            if(element != null) {
-                Size size = stretch ? element.negotiateSize(cell.getSize()) : element.getPreferredSize();
-                element.setBounds(new Rect(size).anchor(cell, cellAlignment)).render();
-            }
-
-            if(x >= shape.getWidth() - 1) {
-                x = 0;
-                cellLeft = cellLeft.anchor(cellLeft.grow(gutterPadding), flow.withCol(1), true);
-                cell = cellLeft;
-            } else {
-                ++x;
-                cell = cell.anchor(cell.grow(gutterPadding), flow.withRow(1), true);
+                if (cell != null) {
+                    Rectangle cellBounds = new Rectangle(
+                        bounds.getPosition().add(cellStep.scale(new Point(row, column))),
+                        cellSize
+                    );
+                    cell.setBounds(cellBounds);
+                }
             }
         }
+    }
+
+    /**
+     * Converts the row count and column count to a double-precision point.
+     * @return The size of the grid in cells.
+     */
+    private Point getShape() {
+        return new Point(columnCount, rowCount);
+    }
+
+    private Point getCellSize() {
+        return Point.max(cells.stream().map(LayoutBox::getPreferredSize)::iterator);
+    }
+
+    /**
+     * Converts the row and column gutters to a point.
+     * @return The size of the gutter.
+     */
+    private Point getGutter() {
+        return new Point(columnGutter, rowGutter);
     }
 }
