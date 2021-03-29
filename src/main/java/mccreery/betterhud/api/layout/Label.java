00e022685e11efdbc49351b0cb23caae965297cb
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-03-29 03:45:24 +0100

    Fix and simplify label

--- src/main/java/mccreery/betterhud/api/layout/Label.java
+++ src/main/java/mccreery/betterhud/api/layout/Label.java
@@ -1,91 +1,41 @@
 package mccreery.betterhud.api.layout;
 
-import static jobicade.betterhud.BetterHud.MC;
+import mccreery.betterhud.api.ScreenRenderContext;
+import mccreery.betterhud.api.geometry.Point;
+import mccreery.betterhud.api.geometry.Rectangle;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 
-import com.mojang.blaze3d.systems.RenderSystem;
+public class Label extends LayoutBox {
+    private final String text;
+    private final int color;
+    private final boolean shadow;
 
-import jobicade.betterhud.geom.Direction;
-import jobicade.betterhud.geom.Point;
-import jobicade.betterhud.geom.Rect;
-import jobicade.betterhud.geom.Size;
-import jobicade.betterhud.util.GlUtil;
-import net.minecraft.client.gui.AbstractGui;
-
-public class Label extends DefaultBoxed {
-    private String text;
-    private Size size;
-    private boolean shadow = true;
-    private Color color = Color.WHITE;
-    private Color background;
-
-    public Label(String text) {
-        setText(text);
-    }
-
-    public Color getBackground() {
-        return background;
-    }
-
-    public void setBackground(Color background) {
-        this.background = background;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public Label setText(String text) {
+    public Label(ScreenRenderContext context, String text, int color, boolean shadow) {
+        super(context);
         this.text = text;
-        this.size = new Size(MC.fontRenderer.getStringWidth(text), MC.fontRenderer.FONT_HEIGHT);
-        return this;
-    }
-
-    public boolean hasShadow() {
-        return shadow;
-    }
-
-    public Label setShadow(boolean shadow) {
-        this.shadow = shadow;
-        return this;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public Label setColor(Color color) {
         this.color = color;
-        return this;
+        this.shadow = shadow;
     }
 
     @Override
-    public Size negotiateSize(Point size) {
-        return this.size;
+    public Point getPreferredSize() {
+        TextRenderer textRenderer = getContext().getTextRenderer();
+        return new Point(textRenderer.getWidth(text), textRenderer.fontHeight);
     }
 
     @Override
     public void render() {
-        if (background != null) {
-            GlUtil.drawRect(bounds, background);
-        }
-
-        /*
-        * The font renderer for some reason ignores our request
-        * and renders completely opaque if opacity < 4.
-        */
-        if(color.getAlpha() < 4) return;
-
-        Point position = new Rect(size).anchor(bounds, Direction.CENTER).getPosition();
+        TextRenderer textRenderer = getContext().getTextRenderer();
+        MatrixStack matrixStack = getContext().getMatrixStack();
+        Rectangle bounds = getBounds();
+        float x = (float)bounds.getX();
+        float y = (float)bounds.getY();
 
         if (shadow) {
-            MC.fontRenderer.drawStringWithShadow(text, position.getX(), position.getY(), color.getPacked());
+            textRenderer.drawWithShadow(matrixStack, text, x, y, color);
         } else {
-            MC.fontRenderer.drawString(text, position.getX(), position.getY(), color.getPacked());
+            textRenderer.draw(matrixStack, text, x, y, color);
         }
-
-        // Restore OpenGL state as expected
-        Color.WHITE.apply();
-        MC.getTextureManager().bindTexture(AbstractGui.GUI_ICONS_LOCATION);
-        RenderSystem.disableAlphaTest();
     }
 }
